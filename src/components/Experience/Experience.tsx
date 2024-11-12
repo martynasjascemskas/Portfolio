4245b7be87b729f82bf0edaaf1fb8ec06ac15d8f
Martynas <martis.jascemskas@gmail.com>
2024-11-12 19:16:26 +0000

    adding experience details

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -8,46 +8,52 @@ function Experience() {
     showDetails(!details);
   };
   return (
-    <>
-      <h1>Experience</h1>
+    <div className="flex flex-col w-full relative items-center justify-center text-center md:gap-6 lg:text-left xl:gap-8 2xl:gap-10">
+      <h1 className="font-bold text-5xl">Experience</h1>
+      <hr className="w-8/12 border-t-4 border-gray-300" />
       {experiences.map((experience, index) => (
-        <div key={index}>
-          <h3>
-            <a
-              href={experience.url}
-              target="_blank"
-              className="company"
-              style={{ color: "white", textDecoration: "underline" }}
-            >
-              {experience.company}
-            </a>
-          </h3>
-          <h3>{experience.title}</h3>
-          <h3>{experience.location}</h3>
-          <h3>{experience.date}</h3>
-          <h3>{experience.type}</h3>
-          <button onClick={handleChange}>
-            {details ? "Hide" : "Show"} Details
-          </button>
-          <Box
-            sx={{
-              display: "flex",
-              height: details ? "150px" : "0px",
-              overflow: "hidden",
-              transition: "height 300ms ease",
-            }}
-          >
-            <Fade in={details}>
-              <ul>
-                {experience.details.map((detail, i) => (
-                  <li key={i}>{detail}</li>
-                ))}
-              </ul>
-            </Fade>
-          </Box>
+        <div key={index} className="flex">
+          <div className="flex flex-col max-w-5xl">
+            <span className="font-extrabold text-2xl">
+              <a href={experience.url} target="_blank" className="company">
+                {experience.company}
+              </a>
+            </span>
+            <span className="font-semibold text-lg">{experience.title}</span>
+            <div>
+              <button
+                onClick={handleChange}
+                className="underline underline-offset-4"
+              >
+                {details ? "Hide" : "Show"} Details
+              </button>
+              <Box
+                sx={{
+                  display: "flex",
+                  height: details ? "150px" : "0px",
+                  maxWidth: "75%",
+                  overflow: "hidden",
+                  transition: "height 500ms ease",
+                }}
+              >
+                <Fade in={details}>
+                  <ul>
+                    {experience.details.map((detail, i) => (
+                      <li key={i}>{detail}</li>
+                    ))}
+                  </ul>
+                </Fade>
+              </Box>
+            </div>
+          </div>
+          <div className="flex flex-col items-end text-right">
+            <span>{experience.date}</span>
+            <span>{experience.location}</span>
+            <span>{experience.type}</span>
+          </div>
         </div>
       ))}
-    </>
+    </div>
   );
 }
 
